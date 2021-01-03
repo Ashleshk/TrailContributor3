de558b3ee6291c1e1494494d9b989492e45db7af
Seemaila-khan <Seemailakhan123@gmail.com>
2021-01-03 09:05:06 +0530

    4 components commited

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ErrorComponent } from './error/error.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { RegisterComponent } from './register/register.component';
-import { SuccessComponent } from './success/success.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ForgotUserIDComponent } from './components/forgot-user-id/forgot-user-id.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SetNewPswComponent } from './components/set-new-psw/set-new-psw.component';
+//import { ErrorComponent } from './error/error.component';
+//import { HomeComponent } from './home/home.component';
+//import { LoginComponent } from '.components/login-page/login-page.component';
+//import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+//import { RegisterComponent } from './register/register.component';
+///import { SuccessComponent } from './success/success.component';
 
 const routes: Routes = [
-  {path:'',component:LoginComponent},
-  {path: 'Home',component:HomeComponent},
-  {path:'Login',component:LoginComponent},
-  {path:'Register',component:RegisterComponent},
-  {path:'Success',component:SuccessComponent},
-  {path:'Error',component:ErrorComponent},
-  {path:'**',component:PageNotFoundComponent} //If Any other links is typed by user
+  {path:'Login',component:LoginPageComponent},
+  {path:'ForgotUserId',component:ForgotUserIDComponent},
+  {path:'ForgotPassword',component:ForgotPasswordComponent},
+  {path:'SetNewPassword',component:SetNewPswComponent} //If Any other links is typed by user
     // above line (pageNotFound) must be last line Always
 ];
 
@@ -22,4 +23,6 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
